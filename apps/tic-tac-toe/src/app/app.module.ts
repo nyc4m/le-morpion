ab0465c2d3a34f6e37ec97c715dcb589ba0ac6d0
nyc4m <baptiste.prunot@protonmail.com>
2021-04-14 21:58:17 +0200

    add toolbar

--- apps/tic-tac-toe/src/app/app.module.ts
+++ apps/tic-tac-toe/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CellContainerDirective } from './cell-container/cell-container.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, CellContainerDirective],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
